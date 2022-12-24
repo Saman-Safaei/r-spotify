a5416b17d70125e9d2fc559161aac3dfec3a29c3
Saman-Safaei <xsamansafaeix@gmail.com>
2022-12-24 13:51:37 +0330

    import tailwind.css file in global scope ( index.js )

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import ReactRouter from "./router/ReactRouter";
+import "./assets/styles/tailwind.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<ReactRouter />);
