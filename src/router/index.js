1d369c88979d60a00db35743094b79a1c7f72fb0
Saman-Safaei <xsamansafaeix@gmail.com>
2022-12-26 19:09:04 +0330

    use DefaultLayout in react router index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import DefaultLayout from "../layouts/DefaultLayout";
 import Home from "../pages/Home";
 
-/** @type {[import("react-router-dom").RouteProps]} */
-export const routes = [{ path: "/", element: <Home /> }];
+/** @type {[import("react-router-dom").RouteObject]} */
+export const routes = [
+  {
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        index: true,
+      },
+    ],
+  },
+];
 
 export default createBrowserRouter(routes);
