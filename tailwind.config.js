aabe09c8f2cbc98b96a31e344b27cfcdf8f3b71a
Saman-Safaei <xsamansafaeix@gmail.com>
2022-12-27 16:33:06 +0330

    add src folder to tailwind contents + add custom spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'lg-sidebar': '16rem',
+        sidebar: '4rem',
+      },
+    },
   },
   plugins: [],
 }
