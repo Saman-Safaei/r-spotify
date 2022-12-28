578dcc6b60b45b15c1e2421d23cec38701bac8a9
Saman-Safaei <xsamansafaeix@gmail.com>
2022-12-28 19:55:32 +0330

    create a basic navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <div className='fixed top-0 left-0 right-0 lg:left-lg-sidebar h-navbar bg-black text-white flex flex-row items-stretch justify-between'>
+      <div className="h-full"></div>
+      <div className="h-full flex flex-row items-center px-4 font-bold">
+        <button className="rounded-full py-3 px-6 text-gray-300">Sign up</button>
+        <button className="rounded-full py-3 px-6 bg-white text-gray-900">Log in</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
