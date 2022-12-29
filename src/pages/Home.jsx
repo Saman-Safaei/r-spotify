72318f7d9858f747020004ebcaaa8a58893e5913
Saman-Safaei <xsamansafaeix@gmail.com>
2022-12-29 09:42:48 +0330

    add a simple playlist to HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,48 @@
+import PlayListContainer from '../components/Playlist/PlayListContainer'
+import PlayListItem from '../components/Playlist/PlayListItem'
+
 function Home() {
-  return ( <div>Home Page</div> );
+  return (
+    <div className='py-4 px-6'>
+      <PlayListContainer title='Focus' href='/category/2341'>
+        <PlayListItem
+          title='Deep Focus'
+          description='Keep calm and focus with ambient and post-rock ...'
+          href='/playlist/65436'
+          imgSrc='/images/deepfocus.jpg'
+          imgAlt='deep-focus'
+        />
+        <PlayListItem
+          title='Deep Focus'
+          description='Keep calm and focus with ambient and post-rock ...'
+          href='/playlist/65436'
+          imgSrc='/images/deepfocus.jpg'
+          imgAlt='deep-focus'
+        />
+        <PlayListItem
+          title='Deep Focus'
+          description='Keep calm and focus with ambient and post-rock ...'
+          href='/playlist/65436'
+          imgSrc='/images/deepfocus.jpg'
+          imgAlt='deep-focus'
+        />
+        <PlayListItem
+          title='Deep Focus'
+          description='Keep calm and focus with ambient and post-rock ...'
+          href='/playlist/65436'
+          imgSrc='/images/deepfocus.jpg'
+          imgAlt='deep-focus'
+        />
+        <PlayListItem
+          title='Deep Focus'
+          description='Keep calm and focus with ambient and post-rock ...'
+          href='/playlist/65436'
+          imgSrc='/images/deepfocus.jpg'
+          imgAlt='deep-focus'
+        />
+      </PlayListContainer>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
