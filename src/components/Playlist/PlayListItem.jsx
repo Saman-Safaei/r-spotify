e676f41be4e509f41461c137c36c8f3450fbd220
Saman-Safaei <xsamansafaeix@gmail.com>
2022-12-29 09:41:16 +0330

    create PlayList Item element

---
+++ src/components/Playlist/PlayListItem.jsx
@@ -0,0 +1,26 @@
+import { PlayIcon } from '@heroicons/react/24/solid'
+import { Link } from 'react-router-dom'
+
+export default function PlayListItem({
+  imgSrc = '',
+  imgAlt = '',
+  title = '',
+  description = '',
+  href = '/',
+}) {
+  return (
+    <div className='relative group flex flex-col items-stretch gap-4 p-4 rounded-lg bg-neutral-800/50 hover:bg-neutral-800 transition-colors duration-300'>
+      <div className='relative w-full'>
+        <img src={imgSrc} alt={imgAlt} className='w-full rounded-md' />
+        <button className='h-12 w-12 p-3 cursor-default z-10 rounded-full bg-green-500 absolute bottom-2 opacity-0 group-hover:bottom-4 group-hover:opacity-100 right-4 transition-all duration-300'>
+          <PlayIcon className='w-full h-full text-black' />
+        </button>
+      </div>
+      <div className='px-1'>
+        <h4 className='mb-1 font-bold text-white'>{title}</h4>
+        <p className='text-sm text-gray-400'>{description}</p>
+      </div>
+      <Link to={href} className='absolute inset-0'></Link>
+    </div>
+  )
+}
