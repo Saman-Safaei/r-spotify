def402cc0618ab4918a0ad833705e9f5864ae077
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-01 14:57:49 +0330

    wrap ReactRouter with UIProvider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ReactRouter from "./router/ReactRouter";
-import "./assets/styles/tailwind.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ReactRouter from './router/ReactRouter'
+import UIProvider from './contexts/ui/UIProvider'
+import './assets/styles/tailwind.css'
+import './assets/styles/transitions.css'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ReactRouter />);
-
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <UIProvider>
+    <ReactRouter />
+  </UIProvider>
+)
