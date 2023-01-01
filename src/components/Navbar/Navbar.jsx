df9fe81f03c9a663cad14e1b870ef986194d5558
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-01 14:57:31 +0330

    add click listener for sign in and sign up buttons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
+import { useContext } from 'react'
+import uiSlice from '../../contexts/ui/ui-slice'
+
 function Navbar() {
+  const uiCtx = useContext(uiSlice)
+
   return (
     <div className='fixed top-0 left-0 right-0 lg:left-lg-sidebar h-navbar bg-black text-white flex flex-row items-stretch justify-between z-50'>
-      <div className="h-full"></div>
-      <div className="h-full flex flex-row items-center px-4 font-bold">
-        <button className="rounded-full py-3 px-6 text-gray-300">Sign up</button>
-        <button className="rounded-full py-3 px-6 bg-white text-gray-900">Log in</button>
+      <div className='h-full'></div>
+      <div className='h-full flex flex-row items-center px-4 font-bold'>
+        <button
+          className='rounded-full py-3 px-6 text-gray-300'
+          onClick={uiCtx.showSignup}>
+          Sign up
+        </button>
+        <button
+          className='rounded-full py-3 px-6 bg-white text-gray-900'
+          onClick={uiCtx.showSignin}>
+          Log in
+        </button>
       </div>
     </div>
   )
