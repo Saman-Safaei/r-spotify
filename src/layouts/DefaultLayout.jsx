1c0740a513c1513b7ca6ec899b3735f9f186414a
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-01 14:57:06 +0330

    add sign up and sign in modal in default layout

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,9 +1,14 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useContext } from 'react'
 import { Outlet } from 'react-router-dom'
+import SigninModal from '../components/Authentication/SigninModal'
+import SignupModal from '../components/Authentication/SignupModal'
 import Navbar from '../components/Navbar/Navbar'
 import Sidebar from '../components/Sidebar/Sidebar'
+import uiSlice from '../contexts/ui/ui-slice'
 
 function DefaultLayout() {
+  const uiCtx = useContext(uiSlice)
+
   return (
     <Fragment>
       <Navbar />
@@ -11,6 +16,8 @@ function DefaultLayout() {
       <main className='pb-sidebar lg:pb-0 mt-navbar lg:ml-lg-sidebar max-w-7xl text-gray-200'>
         <Outlet />
       </main>
+      <SignupModal show={uiCtx.isSignupShow} onHide={uiCtx.hideSignup} />
+      <SigninModal show={uiCtx.isSigninShow} onHide={uiCtx.hideSignin} />
     </Fragment>
   )
 }
