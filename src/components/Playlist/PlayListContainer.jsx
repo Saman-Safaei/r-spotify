c8e55a7807a240fdd5457e9a336bea8a857bb282
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-02 16:34:33 +0330

    make music items more responsive on small devices

--- src/components/Playlist/PlayListContainer.jsx
+++ src/components/Playlist/PlayListContainer.jsx
@@ -11,7 +11,7 @@ export default function PlayListContainer({ children, title = '', href = '' }) {
           Show All
         </Link>
       </div>
-      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4'>
         {children}
       </div>
     </div>
