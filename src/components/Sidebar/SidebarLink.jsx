7ab4f35dc4b64da01dcba9ccd28bef628a259625
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-02 16:38:39 +0330

    add transition to sidebar links

--- src/components/Sidebar/SidebarLink.jsx
+++ src/components/Sidebar/SidebarLink.jsx
@@ -3,11 +3,12 @@ import { Link, useMatch } from 'react-router-dom'
 function SidebarLink({ icon, title, path = '#' }) {
   const isPathMatch = useMatch(path)
   const textColor = isPathMatch ? 'text-gray-100' : 'text-gray-400'
+
   return (
     <li>
       <Link
         to={path}
-        className={`flex flex-row items-center gap-3 ${textColor}`}>
+        className={`flex flex-row items-center gap-3 ${textColor} transition-colors duration-300`}>
         {icon}
         <span className='text-sm hidden lg:inline-block'>{title}</span>
       </Link>
