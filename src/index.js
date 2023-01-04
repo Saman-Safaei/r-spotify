98fdfb896afce6bae0ff9ee95daa5be43b95abc7
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-04 22:59:08 +0330

    wrap react app with UserProvider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ReactRouter from './router/ReactRouter'
 import UIProvider from './contexts/ui/UIProvider'
+import UserProvider from './contexts/user/UserProvider'
 import './assets/styles/tailwind.css'
 import './assets/styles/transitions.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <UIProvider>
-    <ReactRouter />
-  </UIProvider>
+  <UserProvider>
+    <UIProvider>
+      <ReactRouter />
+    </UIProvider>
+  </UserProvider>
 )
