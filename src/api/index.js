b3cf085ea1010ff26fe07563d8eae3d3b63f8dfe
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-04 22:58:11 +0330

    replace hard coded string with environment variable

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
-const baseUrl = 'http://majidsuzoki-001-site1.etempurl.com/api'
+const baseUrl = process.env.REACT_APP_API_URL
 
 /**
  * @param {string} endpoint
  * @param {RequestInit} options
- * @returns Response Promise
+ * @returns ResponsePromise
  */
 export function fetcher(endpoint, options) {
   return fetch(`${baseUrl}${endpoint}`, options)
