5811fdf669260c40cdbea7e4c6bfc816103e03f9
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-06 19:51:30 +0330

    create a query client and wrap app with query client

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import UIProvider from './contexts/ui/UIProvider'
 import UserProvider from './contexts/user/UserProvider'
 import './assets/styles/tailwind.css'
 import './assets/styles/transitions.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient({ mutationCache: false })
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
-  <UserProvider>
-    <UIProvider>
-      <ReactRouter />
-    </UIProvider>
-  </UserProvider>
+  <QueryClientProvider client={queryClient}>
+    <UserProvider>
+      <UIProvider>
+        <ReactRouter />
+      </UIProvider>
+    </UserProvider>
+  </QueryClientProvider>
 )
