125546a7a6675f1bfe34aef11e4335bffbbe0dfa
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-23 05:22:59 +0330

    refactor music api codes and rename pageParam

--- src/api/music.js
+++ src/api/music.js
@@ -1,5 +1,7 @@
 import fetcher from '.'
 
-export const getBySkip = skipNumber => fetcher.get(`/Music/GetBySkip?Skip=${skipNumber}`).then(res => res.data)
+export const getBySkip = ({ pageParam = 0 }) =>
+  fetcher.get(`/Music/GetBySkip?Skip=${pageParam}`).then(res => res.data)
 
-export const getById = musicId => fetcher.get(`/Music/GetById?Id=${musicId}`).then(res => res.data)
+export const getById = ({ pageParam = 0 }) =>
+  fetcher.get(`/Music/GetById?Id=${pageParam}`).then(res => res.data)
