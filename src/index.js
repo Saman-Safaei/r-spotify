29ff5ee0b4c63b2be8ceb9b87c257809298a1a05
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-26 01:45:05 +0330

    add MusicProvider to app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ReactRouter from './router/ReactRouter'
 import UIProvider from './contexts/ui/UIProvider'
 import UserProvider from './contexts/user/UserProvider'
+import MusicProvider from './contexts/music/MusicProvider'
 import './assets/styles/tailwind.css'
 import './assets/styles/transitions.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient({ mutationCache: false })
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -13,9 +14,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <QueryClientProvider client={queryClient}>
     <UserProvider>
-      <UIProvider>
-        <ReactRouter />
-      </UIProvider>
+      <MusicProvider>
+        <UIProvider>
+          <ReactRouter />
+        </UIProvider>
+      </MusicProvider>
     </UserProvider>
   </QueryClientProvider>
 )
