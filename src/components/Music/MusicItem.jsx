fd1f3623aeb36b3bf3df8c9a6648307932c0bd9e
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-26 01:43:37 +0330

    create MusicItem for displaying musics

---
+++ src/components/Music/MusicItem.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom'
+
+function MusicItem({ id, duration, imageCover, title }) {
+  return (
+    <div className='relative flex flex-col items-stretch gap-4 p-4 rounded-lg bg-neutral-800/50 hover:bg-neutral-800 transition-colors duration-300'>
+      <div>
+        <img
+          className='aspect-square w-full object-cover rounded bg-neutral-700/50'
+          src={`https://spotify.storage.iran.liara.space/${imageCover}`}
+          alt={title}
+        />
+      </div>
+      <div>
+        <h3 dir='auto' className='font-bold'>
+          {title}
+        </h3>
+      </div>
+      <div className='mt-auto'>
+        <h4 className='text-xs text-gray-500'>Duration: {duration}sec</h4>
+      </div>
+      <Link to={`/song/${id}`} className='absolute inset-0 z-[1]'></Link>
+    </div>
+  )
+}
+
+export default MusicItem
