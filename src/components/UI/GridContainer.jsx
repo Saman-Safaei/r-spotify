407d94d5d260d027a2c911f3e56d410bfc073b44
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-26 01:43:16 +0330

    create a grid container ui

---
+++ src/components/UI/GridContainer.jsx
@@ -0,0 +1,7 @@
+export default function GridContainer({ children }) {
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4'>
+      {children}
+    </div>
+  )
+}
