fafd25c7b516dfc2e28ee8c57387ce3b23c59dfb
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-26 01:44:49 +0330

    add 3 pages to react router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom'
 import DefaultLayout from '../layouts/DefaultLayout'
 import ErrorPage from '../pages/ErrorPage'
+import Genre from '../pages/Genre'
 import Home from '../pages/Home'
+import Playlist from '../pages/Playlist'
 import Search from '../pages/Search'
+import Song from '../pages/Song'
 
 /** @type {[import("react-router-dom").RouteObject]} */
 export const routes = [
@@ -18,6 +21,18 @@ export const routes = [
       {
         path: '/search',
         element: <Search />,
+      },
+      {
+        path: '/playlist/:id',
+        element: <Playlist />,
+      },
+      {
+        path: '/genre/:name',
+        element: <Genre />,
+      },
+      {
+        path: '/song/:id',
+        element: <Song />,
       },
     ],
   },
