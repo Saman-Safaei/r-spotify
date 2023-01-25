8511177aa512a324968bc2bf7eef0cd673226382
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-26 01:44:14 +0330

    create MusicBar

---
+++ src/components/Music/MusicBar.jsx
@@ -0,0 +1,33 @@
+import { useCallback, useContext } from 'react'
+import { PlayIcon, ForwardIcon, BackwardIcon, PauseIcon } from '@heroicons/react/24/solid'
+import musicSlice from '../../contexts/music/music-slice'
+
+function MusicBar() {
+  const musicCtx = useContext(musicSlice)
+
+  const musicToggleHandler = useCallback(() => {
+    if (musicCtx.isPlaying) musicCtx.pauseMusic()
+    else musicCtx.playMusic()
+  }, [musicCtx.isPlaying])
+
+  return (
+    <div className='fixed bottom-sidebar lg:bottom-0 right-0 left-0 lg:left-lg-sidebar bg-black px-8 py-4 z-10'>
+      <div className='flex flex-row justify-between lg:justify-center lg:gap-6 pb-5'>
+        <button className='w-8 h-8'>
+          <BackwardIcon className='w-full h-full' />
+        </button>
+        <button onClick={musicToggleHandler} className='w-8 h-8'>
+          {musicCtx.isPlaying ? <PauseIcon className='w-full h-full' /> : <PlayIcon className='w-full h-full' />}
+        </button>
+        <button className='w-8 h-8'>
+          <ForwardIcon className='w-full h-full' />
+        </button>
+      </div>
+      <div className='w-full mx-auto h-1 bg-neutral-900'>
+        <div className='w-1/2 h-full bg-green-500'></div>
+      </div>
+    </div>
+  )
+}
+
+export default MusicBar
