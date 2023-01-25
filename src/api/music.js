aeb207829d13014295c0fd742a349fb19970d776
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-26 01:38:26 +0330

    change pageParam object to id for getById function

--- src/api/music.js
+++ src/api/music.js
@@ -3,5 +3,7 @@ import fetcher from '.'
 export const getBySkip = ({ pageParam = 0 }) =>
   fetcher.get(`/Music/GetBySkip?Skip=${pageParam}`).then(res => res.data)
 
-export const getById = ({ pageParam = 0 }) =>
-  fetcher.get(`/Music/GetById?Id=${pageParam}`).then(res => res.data)
+export const getById = id =>
+  fetcher.get(`/Music/GetById?Id=${id}`).then(res => res.data)
+
+
