67843c0b484bd3ab54879534df04b8745c189713
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-26 10:06:05 +0330

    add functionality for music timeline

--- src/contexts/music/MusicProvider.jsx
+++ src/contexts/music/MusicProvider.jsx
@@ -3,11 +3,15 @@ import musicSlice, { actionTypes, defaultValues } from './music-slice'
 
 function musicReducer(state, action) {
   if (action.type === actionTypes.LOAD_MUSIC) {
-    return { ...state, musicUrl: action.payload.url, isPlaying: true }
+    return { ...state, musicUrls: [action.payload.url], isPlaying: true }
   } else if (action.type === actionTypes.PLAY_MUSIC) {
     return { ...state, isPlaying: true }
   } else if (action.type === actionTypes.PAUSE_MUSIC) {
     return { ...state, isPlaying: false }
+  } else if (action.type === actionTypes.INIT_DURATION) {
+    return { ...state, currentDuration: action.payload.duration }
+  } else if (action.type === actionTypes.UPDATE_TIME) {
+    return { ...state, currentTime: action.payload.time }
   } else return state
 }
 
@@ -18,6 +22,7 @@ export default function MusicProvider({ children }) {
   const loadMusicHandler = url => {
     dispatchMusic({ type: actionTypes.LOAD_MUSIC, payload: { url } })
     musicApi.current.src = url
+    dispatchMusic({ type: actionTypes.INIT_DURATION, payload: { duration: musicApi.current.duration } })
     musicApi.current.play().then()
   }
   const playMusicHandler = () => {
@@ -31,12 +36,24 @@ export default function MusicProvider({ children }) {
     musicApi.current.pause()
   }
 
+  musicApi.current.addEventListener('loadedmetadata', () => {
+    dispatchMusic({ type: actionTypes.INIT_DURATION, payload: { duration: musicApi.current.duration } })
+    console.log(musicApi.current.duration)
+  })
+
+  musicApi.current.addEventListener('timeupdate', () => {
+    dispatchMusic({ type: actionTypes.UPDATE_TIME, payload: { time: musicApi.current.currentTime } })
+    console.log(musicApi.current.currentTime)
+  })
+
   const providerValue = {
-    musicUrl: musicState.musicUrl,
+    musicUrls: musicState.musicUrls,
     isPlaying: musicState.isPlaying,
     loadMusic: loadMusicHandler,
     playMusic: playMusicHandler,
     pauseMusic: pauseMusicHandler,
+    currentDuration: musicState.currentDuration,
+    currentTime: musicState.currentTime
   }
 
   return <musicSlice.Provider value={providerValue}>{children}</musicSlice.Provider>
