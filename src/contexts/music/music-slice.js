73da5c62883351bfb6bdecdda572c30d5d4585c2
Saman-Safaei <xsamansafaeix@gmail.com>
2023-02-06 17:27:22 +0330

    add support for play playlists + forward 5 secs music

--- src/contexts/music/music-slice.js
+++ src/contexts/music/music-slice.js
@@ -8,18 +8,21 @@ export const defaultValues = {
 }
 export const defaultFunctions = {
   loadMusic: url => {},
+  loadPlaylist: urls => {},
   playMusic: () => {},
   pauseMusic: () => {},
+  forwardMusic: () => {}
 }
 
 const context = createContext({ ...defaultFunctions, ...defaultValues })
 
 export const actionTypes = {
   LOAD_MUSIC: 'LOAD_MUSIC',
+  LOAD_PLAYLIST: 'LOAD_PLAYLIST',
   PLAY_MUSIC: 'PLAY_MUSIC',
   PAUSE_MUSIC: 'PAUSE_MUSIC',
   INIT_DURATION: 'INIT_DURATION',
-  UPDATE_TIME: "UPDATE_TIME"
+  UPDATE_TIME: 'UPDATE_TIME',
 }
 
 export default context
