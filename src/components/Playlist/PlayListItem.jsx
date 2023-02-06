c1d39a3e67fe56e047c5e91d4a5c1e4e1a90dbe2
Saman-Safaei <xsamansafaeix@gmail.com>
2023-02-06 17:28:54 +0330

    make playlists playable

--- src/components/Playlist/PlayListItem.jsx
+++ src/components/Playlist/PlayListItem.jsx
@@ -1,5 +1,7 @@
+import { useContext } from 'react'
 import { PlayIcon } from '@heroicons/react/24/solid'
 import { Link } from 'react-router-dom'
+import musicSlice from '../../contexts/music/music-slice'
 
 export default function PlayListItem({
   imgSrc = '',
@@ -8,14 +10,18 @@ export default function PlayListItem({
   description = '',
   href = '/',
   playable = true,
+  musicList = [],
 }) {
+  const musicCtx = useContext(musicSlice)
   return (
     <div className='relative group flex flex-col items-stretch gap-4 p-4 rounded-lg bg-neutral-800/50 hover:bg-neutral-800 transition-colors duration-300'>
       <div className='relative w-full'>
         <img src={imgSrc} alt={imgAlt} className='w-full rounded-md' />
-        { playable && <button className='h-12 w-12 p-3 cursor-default z-10 rounded-full bg-green-500 absolute bottom-4 md:bottom-2 md:opacity-0 md:group-hover:bottom-4 md:group-hover:opacity-100 right-4 transition-all duration-300'>
-          <PlayIcon className='w-full h-full text-black' />
-        </button> }
+        {playable && (
+          <button onClick={() => musicCtx.loadPlaylist(musicList)} className='h-12 w-12 p-3 cursor-default z-10 rounded-full bg-green-500 absolute bottom-4 md:bottom-2 md:opacity-0 md:group-hover:bottom-4 md:group-hover:opacity-100 right-4 transition-all duration-300'>
+            <PlayIcon className='w-full h-full text-black' />
+          </button>
+        )}
       </div>
       <div className='px-1'>
         <h4 className='mb-1 font-bold text-white'>{title}</h4>
