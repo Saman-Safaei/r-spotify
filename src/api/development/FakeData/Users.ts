1d7e0f4ccbd8f8649d46b70cf8abb58c6cf17613
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-02 19:05:47 +0330

    feat: add some fake data for msw api
    Users fake data added to Users.ts

---
+++ src/api/development/FakeData/Users.ts
@@ -0,0 +1,22 @@
+import type { MockUser } from '../../../types/MockUser';
+
+const users: MockUser[] = [
+  {
+    id: 0,
+    firstname: 'Saman',
+    lastname: 'Safaei',
+    email: 'xsamansafaeix@gmail.com',
+    username: 'SamanDev',
+    playlists: [],
+  },
+  {
+    id: 1,
+    firstname: 'Ali',
+    lastname: 'Karimi',
+    email: 'AliKarimi@gmail.com',
+    username: 'AliFake',
+    playlists: [],
+  },
+];
+
+export default users;
