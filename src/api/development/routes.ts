51721e06beeffb4611833bd4eb8edaaab677de3e
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-02 19:32:37 +0330

    fix: change signin req method to POST

--- src/api/development/routes.ts
+++ src/api/development/routes.ts
@@ -3,8 +3,8 @@ import url from "./Utilities/filename";
 import {SignIn, SignUp} from "./Controllers/user";
 
 const handlers: RestHandler[] = [
-    rest.get(url`/auth/signin`, SignIn),
-    rest.post(url`/auth/signup`, SignUp)
+    rest.post(url`/auth/signin`, SignIn),
+    rest.post(url`/auth/signup`, SignUp),
 ];
 
 export default handlers;
