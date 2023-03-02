dd0c3bb293a43941ff26ff5adaa5d8565e777966
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-02 19:34:23 +0330

    feat: add getUserInfo route to msw api

--- src/api/development/routes.ts
+++ src/api/development/routes.ts
@@ -1,10 +1,11 @@
 import { rest, RestHandler } from 'msw';
-import url from "./Utilities/filename";
-import {SignIn, SignUp} from "./Controllers/user";
+import url from './Utilities/filename';
+import { getUserInfo, SignIn, SignUp } from './Controllers/user';
 
 const handlers: RestHandler[] = [
-    rest.post(url`/auth/signin`, SignIn),
-    rest.post(url`/auth/signup`, SignUp),
+  rest.post(url`/auth/signin`, SignIn),
+  rest.post(url`/auth/signup`, SignUp),
+  rest.post(url`/auth/getUserInfo`, getUserInfo),
 ];
 
 export default handlers;
