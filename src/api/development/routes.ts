cdba49682c6c6daa58eb877850f2ee8382136f37
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-02 19:04:43 +0330

    feat: add dummy RestHandlers for testing msw api

--- src/api/development/routes.ts
+++ src/api/development/routes.ts
@@ -1,5 +1,10 @@
-import { RestHandler } from 'msw';
+import { rest, RestHandler } from 'msw';
+import url from "./Utilities/filename";
+import {SignIn, SignUp} from "./Controllers/user";
 
-const handlers: RestHandler[] = [];
+const handlers: RestHandler[] = [
+    rest.get(url`/auth/signin`, SignIn),
+    rest.post(url`/auth/signup`, SignUp)
+];
 
 export default handlers;
