61ec08dd9f39afc876935b3bb15ae510e903334f
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-02 18:50:58 +0330

    refactor: remove unused log message

--- src/hooks/signup.js
+++ src/hooks/signup.js
@@ -1,30 +1,28 @@
-import { useMutation } from '@tanstack/react-query'
-import { signup } from '../api/users'
-import useSignin from './signin'
+import { useMutation } from '@tanstack/react-query';
+import { signup } from '../api/users';
+import useSignin from './signin';
 
 export default function useSignup() {
-  const { error, signin, isError, isLoading } = useSignin()
+  const { error, signin, isError, isLoading } = useSignin();
 
   const successHandler = variables => {
-    signin(variables)
-  }
+    signin(variables);
+  };
 
   const { mutate, ...mutationData } = useMutation({
     mutationFn: signup,
-  })
+  });
 
   const signupHandler = variables => {
     mutate(variables, {
       onSuccess: () => successHandler(variables),
-    })
-  }
+    });
+  };
 
   const errorMessage =
     mutationData?.error?.response?.status === 409
       ? 'The Username or email are already exists.'
-      : mutationData?.error?.message
-
-      console.log(mutationData?.error?.status);
+      : mutationData?.error?.message;
 
   return {
     signup: signupHandler,
@@ -32,6 +30,6 @@ export default function useSignup() {
     error: mutationData.error || error,
     isError: mutationData.isError || isError,
     isLoading: mutationData.isLoading || isLoading,
-    errorMessage
-  }
+    errorMessage,
+  };
 }
