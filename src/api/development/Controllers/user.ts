0d9d7ca943674d5c3de4aa36b3b10306119a8080
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-02 19:27:50 +0330

    feat: make signin and signup api works

--- src/api/development/Controllers/user.ts
+++ src/api/development/Controllers/user.ts
@@ -1,9 +1,39 @@
 import { ResponseComposition, RestContext, RestRequest } from 'msw';
+import FakeUsers from '../FakeData/Users';
+import type { MockUser } from '../../../types/MockUser';
+import type { SignUpBody } from '../../../types/MockSIgnUpBody';
+import type { SignInBody } from '../../../types/MockSignInBody';
 
-export async function SignIn(_1: RestRequest, res: ResponseComposition, ctx: RestContext) {
-  return res(ctx.status(200));
+export async function SignIn(req: RestRequest, res: ResponseComposition, ctx: RestContext) {
+  const requestBody = await req.json<SignInBody>();
+  const fakeUser = FakeUsers.find(fakeUser => fakeUser.username === requestBody.username);
+
+  if (!fakeUser) {
+    return res(ctx.status(404));
+  }
+  if (!(fakeUser.password === requestBody.password)) {
+    return res(ctx.status(403));
+  }
+
+  return res(ctx.status(200), ctx.json({ token: fakeUser.id }));
 }
 
-export async function SignUp(_1: RestRequest, res: ResponseComposition, ctx: RestContext) {
-  return res(ctx.status(200));
+export async function SignUp(req: RestRequest, res: ResponseComposition, ctx: RestContext) {
+  const requestBody = await req.json<SignUpBody>();
+
+  const userID = Math.floor(Math.random() * 100000);
+
+  const user: MockUser = {
+    id: userID,
+    email: requestBody.email,
+    playlists: [],
+    username: requestBody.username,
+    firstname: 'John',
+    lastname: 'Doe',
+    password: requestBody.password,
+  };
+
+  FakeUsers.push(user);
+
+  return res(ctx.status(200), ctx.json({ token: userID }));
 }
