e6ee34ff03d9aa4d19b2092e43a14d2c200a572f
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-02 19:07:13 +0330

    feat: run msw in development mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import ReactRouter from './router/ReactRouter'
-import UIProvider from './contexts/ui/UIProvider'
-import UserProvider from './contexts/user/UserProvider'
-import MusicProvider from './contexts/music/MusicProvider'
-import './assets/styles/tailwind.css'
-import './assets/styles/transitions.css'
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ReactRouter from './router/ReactRouter';
+import UIProvider from './contexts/ui/UIProvider';
+import UserProvider from './contexts/user/UserProvider';
+import MusicProvider from './contexts/music/MusicProvider';
+import './assets/styles/tailwind.css';
+import './assets/styles/transitions.css';
 
-const queryClient = new QueryClient()
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement)
+const queryClient = new QueryClient();
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement);
+
+if (process.env.NODE_ENV === 'development') {
+  const { serviceWorker } = require('./api/development/browser');
+  serviceWorker.start({ quiet: false, onUnhandledRequest: 'warn' });
+}
 
 root.render(
   <QueryClientProvider client={queryClient}>
@@ -21,4 +25,4 @@ root.render(
       </MusicProvider>
     </UserProvider>
   </QueryClientProvider>
-)
+);
