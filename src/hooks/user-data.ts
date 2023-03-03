701f4d81f31994f27062d6ebd9da00a500988a17
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-03 15:06:48 +0330

    feat: add useUserData hook
    add useUserData hook for fetch user data with provided token

---
+++ src/hooks/user-data.ts
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+import { userData } from '../api/users';
+import { AxiosError } from 'axios';
+
+export default function useUserData(
+  isLogged: boolean,
+  setUser: (username: string, email: string) => void,
+  logoutUser: () => void
+) {
+  const { error, isError } = useQuery<{ data: any; statusCode: number }, AxiosError>({
+    queryFn: () => userData(localStorage.getItem('token')),
+    refetchInterval: 20000,
+    enabled: isLogged,
+    onSuccess: response => setUser(response.data.username, response.data.email),
+    onError: () => logoutUser(),
+    queryKey: ['userInfo']
+  });
+
+  return { error, isError };
+}
