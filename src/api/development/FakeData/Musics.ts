714419d8a6bdc106bd2feaf72d68652f4f8ee623
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-04 19:56:38 +0330

    feat: add fake data for musics

---
+++ src/api/development/FakeData/Musics.ts
@@ -0,0 +1,52 @@
+const musics: MockMusic[] = [
+  {
+    id: 0,
+    musicFile: 'musics/lord_of_cinder.mp3',
+    like: 0,
+    duration: 324,
+    title: 'Gwyn - The Lord of Cinder',
+    imageCover: 'images/DarkSoulsCover.jpg',
+    publishDate: '1401/04/05',
+    album: 'Dark Souls 1',
+    singer: {
+      id: 0,
+      profilephoto: "images/DarkSoulsCover.jpg",
+      firstname: 'John',
+      lastname: 'Doe',
+    },
+  },
+  {
+    id: 1,
+    musicFile: 'musics/lord_of_cinder.mp3',
+    like: 0,
+    duration: 324,
+    title: 'Gwyn - The Lord of Cinder',
+    imageCover: 'images/DarkSoulsCover.jpg',
+    publishDate: '1401/04/05',
+    album: 'Dark Souls 1',
+    singer: {
+      id: 0,
+      firstname: 'John',
+      lastname: 'Doe',
+      profilephoto: "images/DarkSoulsCover.jpg"
+    },
+  },
+  {
+    id: 2,
+    musicFile: 'musics/lord_of_cinder.mp3',
+    like: 0,
+    duration: 324,
+    title: 'Gwyn - The Lord of Cinder',
+    imageCover: 'images/DarkSoulsCover.jpg',
+    publishDate: '1401/04/05',
+    album: 'Dark Souls 1',
+    singer: {
+      id: 0,
+      profilephoto: "images/DarkSoulsCover.jpg",
+      firstname: 'John',
+      lastname: 'Doe',
+    },
+  },
+];
+
+export default musics;
