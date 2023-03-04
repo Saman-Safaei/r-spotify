35bb7ae4223f89e606b16b9267f9c0e3c6eec8fb
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-04 19:55:12 +0330

    feat: change key and add type to query

--- src/hooks/song-by-id.ts
+++ src/hooks/song-by-id.ts
@@ -1,18 +1,18 @@
-import { useEffect } from 'react'
-import { useQuery } from '@tanstack/react-query'
-import {getById, TGetById} from '../api/music'
-import {AxiosError} from 'axios'
+import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { getById, TGetById } from '../api/music';
+import { AxiosError } from 'axios';
 
-export default function useSongById(id:number) {
-  const { data, isLoading, isError, isSuccess, error } = useQuery<TGetById, AxiosError, TGetById, any[]>({
-    queryKey: ['song', id],
-    queryFn: async () => await getById(id),
-  })
+export default function useSongById(id: number) {
+  const { data, isLoading, isError, isSuccess, error } = useQuery<RQueryData<TGetById>, AxiosError>({
+    queryKey: ['music', id],
+    queryFn: () => getById(id),
+  });
 
   useEffect(() => {
     if (isError && error.response?.status === 404)
-      throw new Response(null, { statusText: 'not found song', status: 404 })
-  }, [error, isError])
+      throw new Response(null, { statusText: 'not found song', status: 404 });
+  }, [error, isError]);
 
-  return { data, isLoading, isError, isSuccess, error }
+  return { data: data?.data, isLoading, isError, isSuccess, error };
 }
