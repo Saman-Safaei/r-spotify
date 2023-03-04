e28f0218b19e464fd5a96fc01c8e5934c05597f3
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-04 19:57:20 +0330

    feat: add music fetcher handlers in routes.ts

--- src/api/development/routes.ts
+++ src/api/development/routes.ts
@@ -1,11 +1,14 @@
 import { rest, RestHandler } from 'msw';
 import url from './Utilities/name';
 import { getUserInfo, SignIn, SignUp } from './Controllers/user';
+import {getMusicById, getMusicsBySkip} from './Controllers/musics';
 
 const handlers: RestHandler[] = [
   rest.post(url`/auth/signin`, SignIn),
   rest.post(url`/auth/signup`, SignUp),
   rest.post(url`/auth/getUserInfo`, getUserInfo),
+  rest.post(url`/musics/getById`, getMusicById),
+  rest.get(url`/musics/getBySkip`, getMusicsBySkip)
 ];
 
 export default handlers;
