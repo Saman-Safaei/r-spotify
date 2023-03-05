7dbbec36166a94715b3582291aac7910ec0e35c1
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-05 20:54:20 +0330

    feat: make play button require auth

--- src/components/Playlist/PlayListItem.tsx
+++ src/components/Playlist/PlayListItem.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { PlayIcon } from '@heroicons/react/24/solid';
 import { Link } from 'react-router-dom';
 import musicSlice from '../../contexts/music/music-slice';
+import { AuthButton } from '../../hocs/with-auth';
 
 interface PlayListItemProps {
   imgSrc: string;
@@ -26,13 +27,13 @@ export default function PlayListItem({
   return (
     <div className='relative group flex flex-col items-stretch gap-4 p-4 rounded-lg bg-neutral-800/50 hover:bg-neutral-800 transition-colors duration-300'>
       <div className='relative w-full'>
-        <img src={imgSrc} alt={imgAlt} className='w-full rounded-md' />
+        <img src={imgSrc} alt={imgAlt} className='w-full rounded-md aspect-square object-center' loading='lazy' />
         {playable && (
-          <button
+          <AuthButton
             onClick={() => musicCtx.loadPlaylist(musicList)}
-            className='h-12 w-12 p-3 cursor-default z-10 rounded-full bg-green-500 absolute bottom-4 md:bottom-2 md:opacity-0 md:group-hover:bottom-4 md:group-hover:opacity-100 right-4 transition-all duration-300'>
+            className='h-12 w-12 p-3 cursor-pointer z-10 rounded-full bg-green-500 absolute bottom-4 md:bottom-2 md:opacity-0 md:group-hover:bottom-4 md:group-hover:opacity-100 right-4 transition-all duration-300'>
             <PlayIcon className='w-full h-full text-black' />
-          </button>
+          </AuthButton>
         )}
       </div>
       <div className='px-1'>
