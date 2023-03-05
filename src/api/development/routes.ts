359022dd62cf472b21887d53caf5056d4017bfcb
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-05 13:03:37 +0330

    feat: add playlists Response-Handlers to routes.ts

--- src/api/development/routes.ts
+++ src/api/development/routes.ts
@@ -1,14 +1,17 @@
 import { rest, RestHandler } from 'msw';
 import url from './Utilities/name';
 import { getUserInfo, SignIn, SignUp } from './Controllers/user';
-import {getMusicById, getMusicsBySkip} from './Controllers/musics';
+import { getMusicById, getMusicsBySkip } from './Controllers/musics';
+import { getPlaylistById, getPlaylistBySkip } from './Controllers/playlist';
 
 const handlers: RestHandler[] = [
   rest.post(url`/auth/signin`, SignIn),
   rest.post(url`/auth/signup`, SignUp),
   rest.post(url`/auth/getUserInfo`, getUserInfo),
   rest.post(url`/musics/getById`, getMusicById),
-  rest.get(url`/musics/getBySkip`, getMusicsBySkip)
+  rest.get(url`/musics/getBySkip`, getMusicsBySkip),
+  rest.get(url`/playlists/getById`, getPlaylistById),
+  rest.get(url`/playlists/getBySkip`, getPlaylistBySkip),
 ];
 
 export default handlers;
