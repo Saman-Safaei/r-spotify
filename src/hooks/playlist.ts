2fd5db44cdea901b2660fed7ba92226166569226
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-06 23:43:10 +0330

    feat: create usePlaylist hook function
    create a hook for fetch a playlist by id

--- src/hooks/playlist.ts
+++ src/hooks/playlist.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
-import { getBySkip } from '../api/playlist';
+import { getById, getBySkip } from '../api/playlist';
+import { AxiosError } from 'axios';
 
 export function usePlaylists(skip: number, count: number, category: string) {
   const { data, isSuccess } = useQuery({
@@ -8,4 +9,24 @@ export function usePlaylists(skip: number, count: number, category: string) {
   });
 
   return { data: data?.data, isSuccess };
+}
+
+export function usePlaylist(id: string) {
+  if (!id || isNaN(+id))
+    throw new Response(null, {
+      status: 400,
+      statusText: 'Invalid Route',
+    });
+
+  const { data: response, isSuccess, error } = useQuery<RQueryData<MockPlaylist>, AxiosError>({
+    queryFn: () => getById(+id),
+    queryKey: ['playlist', +id],
+    retry: false
+  });
+
+  if (error?.response?.status === 404) {
+    throw new Response(null, {status: 404, statusText: "Playlist Not Found"})
+  }
+
+  return { response, isSuccess };
 }
