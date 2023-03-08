dda43ed7d16279258b186c90cf1dc30080cad8fe
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-08 23:54:22 +0330

    feat: add enum type to Types.ts

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -4,3 +4,4 @@
 type Nullable<T> = T | null;
 type RQueryResponse<T = any> = Promise<{ data: T; statusCode: number }>;
 type RQueryData<T = any> = { data: T; statusCode: number };
+type EnumType = string | number;
