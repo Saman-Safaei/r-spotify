5846cdde4f5134f6efb466b9c2926cfc9173cb02
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-10 22:05:57 +0330

    feat: create new generic type for optional values

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -2,6 +2,7 @@
 // @ts-ignore
 
 type Nullable<T> = T | null;
+type Optional<T> = T | undefined;
 type RQueryResponse<T = any> = Promise<{ data: T; statusCode: number }>;
 type RQueryData<T = any> = { data: T; statusCode: number };
 type EnumType = string | number;
