f2504598b2c0e5795629caf9b8f490414d516eb5
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-12 19:30:47 +0330

    feat: add auth token to music requests

--- src/api/music.ts
+++ src/api/music.ts
@@ -14,13 +14,33 @@ export const getBySkip = ({ pageParam = 0 }): RQueryResponse<TGetBySkip[]> =>
   }));
 
 export const getById = (id: number): RQueryResponse<MockMusic> =>
-  fetcher.post(`/musics/getById`, { id }).then(res => ({
-    data: res.data,
-    statusCode: res.status,
-  }));
+  fetcher
+    .post(
+      `/musics/getById`,
+      { id },
+      {
+        headers: {
+          Bearer: localStorage.getItem('token'),
+        },
+      }
+    )
+    .then(res => ({
+      data: res.data,
+      statusCode: res.status,
+    }));
 
 export const setLike = ({ id }: { id: number }): RQueryResponse<undefined> =>
-  fetcher.post('/musics/setLike', { likeId: id }).then(res => ({
-    data: undefined,
-    statusCode: res.status,
-  }));
+  fetcher
+    .post(
+      '/musics/setLike',
+      { likeId: id },
+      {
+        headers: {
+          Bearer: localStorage.getItem('token'),
+        },
+      }
+    )
+    .then(res => ({
+      data: undefined,
+      statusCode: res.status,
+    }));
