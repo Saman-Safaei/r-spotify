794630ed5814cd1bef1147b62f2cd331554ceb4e
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-12 19:32:25 +0330

    feat: add auth to playlist api

--- src/api/playlist.ts
+++ src/api/playlist.ts
@@ -1,13 +1,17 @@
 import fetcher from '.';
 
-export const getBySkip = (skip: number, count: number, category: string): RQueryResponse<MockLimitedPlaylist[]> =>
-  fetcher.get(`/playlists/getBySkip?skip=${skip}&take=${count}&category=${category}`).then(res => ({
+export const getBySkip = (skip: number, count: number, category?: number): RQueryResponse<MockLimitedPlaylist[]> =>
+  fetcher.get(`/playlists/getBySkip?skip=${skip}&take=${count}${category ? `&category=${category}` : ''}`).then(res => ({
     data: res.data,
     statusCode: res.status,
   }));
 
 export const getById = (id: number): RQueryResponse<MockPlaylist> =>
-  fetcher.get(`/playlists/getById?id=${id}`).then(res => ({
+  fetcher.get(`/playlists/getById?id=${id}`, {
+    headers: {
+      Bearer: localStorage.getItem('token'),
+    },
+  }).then(res => ({
     data: res.data,
     statusCode: res.status,
   }));
