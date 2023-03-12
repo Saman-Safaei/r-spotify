d2423f1056be8ad8724ce7203241da359ff9cb3c
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-12 19:35:10 +0330

    feat: add auth to users api with headers

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,19 +1,25 @@
 import fetcher from '.';
 
-export const signup = (signupInfo: any):RQueryResponse =>
+export const signup = (signupInfo: any): RQueryResponse =>
   fetcher.post('/auth/signup', signupInfo).then(res => ({
     data: res.data,
     statusCode: res.status,
   }));
 
-export const signin = (signinInfo: any):RQueryResponse =>
+export const signin = (signinInfo: any): RQueryResponse =>
   fetcher.post('/auth/signin', signinInfo).then(res => ({
     data: res.data,
     statusCode: res.status,
   }));
 
-export const userData = (token: Nullable<string>):RQueryResponse =>
-  fetcher.post('/auth/getUserInfo', { token }).then(res => ({
-    data: res.data,
-    statusCode: res.status,
-  }));
+export const userData = (): RQueryResponse =>
+  fetcher
+    .post('/auth/getUserInfo', undefined, {
+      headers: {
+        Bearer: localStorage.getItem('token'),
+      },
+    })
+    .then(res => ({
+      data: res.data,
+      statusCode: res.status,
+    }));
