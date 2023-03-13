5369c96419cfee6c9d528188f464c09843af763b
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-13 23:20:39 +0330

    feat: make Musics functional with DRY code

--- src/api/development/FakeData/Musics.ts
+++ src/api/development/FakeData/Musics.ts
@@ -1,4 +1,8 @@
-const musics: MockMusic[] = [
+const musics = (
+  albums: MockLimitedAlbum[],
+  categories: MockLimitedCategory[],
+  singers: MockLimitedSinger[]
+): MockMusic[] => [
   {
     id: 0,
     musicFile: 'musics/lord_of_cinder.mp3',
@@ -7,20 +11,9 @@ const musics: MockMusic[] = [
     title: 'Gwyn - The Lord of Cinder',
     imageCover: 'images/DarkSoulsCover.jpg',
     publishDate: '1401/04/05',
-    album: {
-      id: 0,
-      name: 'Dark Souls 1',
-    },
-    category: {
-      id: 0,
-      name: 'Soundtrack',
-    },
-    singer: {
-      id: 0,
-      profilephoto: 'images/DarkSoulsCover.jpg',
-      firstname: 'John',
-      lastname: 'Doe',
-    },
+    album: albums.find(album => album.id === 0)!,
+    category: categories.find(category => category.id === 0)!,
+    singer: singers.find(singer => singer.id === 0)!,
   },
   {
     id: 1,
@@ -30,20 +23,9 @@ const musics: MockMusic[] = [
     title: 'Knight Artorias',
     imageCover: 'images/DarkSoulsCover.jpg',
     publishDate: '1401/04/05',
-    album: {
-      id: 0,
-      name: 'Dark Souls 1',
-    },
-    category: {
-      id: 0,
-      name: 'Soundtrack',
-    },
-    singer: {
-      id: 0,
-      firstname: 'John',
-      lastname: 'Doe',
-      profilephoto: 'images/DarkSoulsCover.jpg',
-    },
+    album: albums.find(album => album.id === 0)!,
+    category: categories.find(category => category.id === 0)!,
+    singer: singers.find(singer => singer.id === 0)!,
   },
   {
     id: 2,
@@ -53,20 +35,9 @@ const musics: MockMusic[] = [
     title: 'Ornstein & Smough',
     imageCover: 'images/DarkSoulsCover.jpg',
     publishDate: '1401/04/05',
-    album: {
-      id: 0,
-      name: 'Dark Souls 1',
-    },
-    category: {
-      id: 0,
-      name: 'Soundtrack',
-    },
-    singer: {
-      id: 0,
-      profilephoto: 'images/DarkSoulsCover.jpg',
-      firstname: 'John',
-      lastname: 'Doe',
-    },
+    album: albums.find(album => album.id === 0)!,
+    category: categories.find(category => category.id === 0)!,
+    singer: singers.find(singer => singer.id === 0)!,
   },
 ];
 
