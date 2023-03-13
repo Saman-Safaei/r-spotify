5898240a060a0bc0a38f80b57e104be2caee9af5
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-13 13:03:02 +0330

    feat: add category controller to urls

--- src/api/development/routes.ts
+++ src/api/development/routes.ts
@@ -3,7 +3,7 @@ import url from './Utilities/name';
 import { getUserInfo, SignIn, SignUp } from './Controllers/user';
 import { getMusicById, getMusicsBySkip, setLike } from './Controllers/musics';
 import { getPlaylistById, getPlaylistBySkip } from './Controllers/playlist';
-import { search } from './Controllers/search';
+import { categories, search } from './Controllers/search';
 
 const handlers: RestHandler[] = [
   rest.post(url`/auth/signin`, SignIn),
@@ -14,7 +14,8 @@ const handlers: RestHandler[] = [
   rest.get(url`/playlist/getById`, getPlaylistById),
   rest.get(url`/playlist/getBySkip`, getPlaylistBySkip),
   rest.post(url`/music/like`, setLike),
-  rest.get(url`/search`, search), // need <q> in query params - ( /search?q=Metal&category=Soundtrack )
+  rest.get(url`/search`, search), // need <q> in query params - ( /search?q=Metal&category=Soundtrack ),
+  rest.get(url`/categories`, categories),
 ];
 
 export default handlers;
