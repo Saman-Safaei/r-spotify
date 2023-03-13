86a4b198ba4d98ee202a6085ed9acb98c0ae49b1
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-13 12:57:07 +0330

    feat: create a function for auto create auth config

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,16 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 const baseUrl = process.env.REACT_APP_API_URL;
 
 export default axios.create({
   baseURL: baseUrl,
 });
+
+export function auth({ ...config }: AxiosRequestConfig): AxiosRequestConfig {
+  return {
+    headers: {
+      Authorization: localStorage.getItem('token'),
+    },
+    ...config,
+  };
+}
