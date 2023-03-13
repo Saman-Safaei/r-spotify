41ce28b3617ffd78ef6664d0092814d25ca5c8bb
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-13 18:07:51 +0330

    feat: fix search request maker and add categories request maker

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,18 +1,22 @@
 import fetcher from '.';
 
-export const search = ({
-  query,
-  category,
-  pageParam
-}: SearchParams): RQueryResponse<(MockPlaylist | MockMusic)[]> => {
-  const url = new URLSearchParams()
+export const search = ({ query, category, pageParam }: SearchParams): RQueryResponse<(MockPlaylist | MockMusic)[]> => {
+  const url = new URLSearchParams();
   if (query) url.set('q', query);
-  if (category) url.set('category', category);
   
+  url.set('category', `${category}`);
   url.set('skip', pageParam.toString());
+  
+  console.log(url.toString())
 
   return fetcher.get(`/search?${url.toString()}`).then(res => ({
     data: res.data,
     statusCode: res.status,
   }));
 };
+
+export const categories = (): RQueryResponse<MockLimitedCategory[]> =>
+  fetcher.get('/categories').then(res => ({
+    data: res.data,
+    statusCode: res.status,
+  }));
