9344659f8cefa85fe001dee0337d9b86c70cdab7
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-13 23:21:56 +0330

    feat: create functional fake singers data

--- src/api/development/FakeData/Singers.ts
+++ src/api/development/FakeData/Singers.ts
@@ -1,1 +1,10 @@
-export {};
+const singers = (): MockLimitedSinger[] => [
+  {
+    id: 0,
+    firstname: 'John',
+    lastname: 'Doe',
+    profilephoto: 'images/DarkSoulsCover.jpg',
+  },
+];
+
+export default singers;
