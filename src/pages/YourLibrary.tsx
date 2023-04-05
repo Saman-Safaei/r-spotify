bfffc1dcaf738d009603a1683550d8447a7e22b1
Saman-Safaei <xsamansafaeix@gmail.com>
2023-04-05 11:16:23 +0430

    feat: initialize YourLibrary.tsx page

---
+++ src/pages/YourLibrary.tsx
@@ -0,0 +1,10 @@
+import { Fragment } from 'react';
+import type { FC } from 'react';
+
+export interface YourLibraryProps {}
+
+const YourLibrary: FC<YourLibraryProps> = () => {
+  return <Fragment></Fragment>;
+};
+
+export default YourLibrary;
